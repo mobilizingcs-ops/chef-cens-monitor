7ee6252fee63f424193c35932c959f7b719c4683
Steve Nolen <technolengy@gmail.com>
2015-02-10 12:07:11 -0800

    change memory alerts

--- recipes/base_checks.rb
+++ recipes/base_checks.rb
@@ -18,8 +18,15 @@
 #
 
 include_recipe "monitor::default"
+#disk percent util and metrics
 include_recipe "monitor::_check-disk"
 include_recipe "monitor::_disk-usage-metrics"
+#memory percent util and metrics
+include_recipe "monitor::_check-memory-pcnt"
+include_recipe "monitor::_memory-metrics"
+#cpu percent util and metrics
+include_recipe "monitor::_check-cpu"
+include_recipe "monitor::_cpu-metrics"
 
 sensu_check "check-disk" do
   command "check-disk.rb -x nfs -w 85 -c 95"
@@ -35,4 +42,34 @@ sensu_check "disk_usage_metrics" do
   handlers ["graphite"]
   standalone true
   interval 300
+end
+
+sensu_check "check-memory-pcnt" do
+  command "check-memory-pcnt.sh -w 95"
+  handlers ["default", "mailer"]
+  standalone true
+  interval 60
+end
+
+sensu_check "memory-metrics" do
+  type "metric"
+  command "memory-metrics.rb"
+  handlers ["graphite"]
+  standalone true
+  interval 30
+end
+
+sensu_check "check-cpu" do
+  command "check-cpu.rb"
+  handlers ["default", "mailer"]
+  standalone true
+  interval 60
+end
+
+sensu_check "cpu-metrics" do
+  type "metric"
+  command "cpu-metrics.rb"
+  handlers ["graphite"]
+  standalone true
+  interval 30
 end
