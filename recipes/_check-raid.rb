2323b33431b5a13cee12666bd2f3965d348b156b
Steve Nolen <technolengy@gmail.com>
2015-02-18 15:22:11 -0800

    the sudoer saga is done

--- recipes/_check-raid.rb
+++ recipes/_check-raid.rb
@@ -28,6 +28,8 @@ apt_repository "hwraid" do
   components ["main"]
 end 
 package "megacli"
+
+node.set['authorization']['sudo']['include_sudoers_d']
 node.set["monitor"]["sudo_commands"] = ["/usr/sbin/megacli -AdpAllInfo -aALL"]
 include_recipe "monitor::_sudo"
 
