a52d447ddc5f9dd4c485d3a6c5763e6da08ecf57
Steve Nolen <technolengy@gmail.com>
2015-02-18 13:47:52 -0800

    add check for hardware raid (needed on physical nodes)

---
+++ recipes/_check-raid.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: monitor
+# Recipe:: _check-raid
+#
+# Copyright 2015, Steve Nolen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "monitor::default"
+
+#add the hwraid repo and install megacli
+include_recipe "apt"
+apt_repository "hwraid" do
+  uri "http://hwraid.le-vert.net/#{node['platform']}"
+  key "http://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key"
+  distribution node['lsb']['codename']
+  components ["main"]
+end 
+package "megacli"
+
+
+cookbook_file "/etc/sensu/plugins/check-raid.rb" do
+  source "plugins/check-raid.rb"
+  mode 0755
+end
